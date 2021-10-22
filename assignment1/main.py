59d0205c55288f8d6d722f86aecee3248627239a
Rita Bogdanova <ritabogdanovashapkina@gmail.com>
2021-10-21 23:06:11 -0400

    final

--- assignment1/main.py
+++ assignment1/main.py
@@ -2,6 +2,8 @@
 import json
 import sys
 import os
+import math 
+import statistics
 
 INPUT_FILE = 'testdata.json' # Constant variables are usually in ALL CAPS
 
@@ -13,11 +15,61 @@ class User:
         self.grad_year = grad_year
         self.responses = responses
 
+def calculate_stats():
+    numUsers = len(users)
+    numQuestions = len(users[0].responses)
+    responsesPer = []
+    
+
+    for i in range(numQuestions):
+        resp = []
+        for j in range(numUsers): 
+            resp.append(users[j].responses[i])
+        responsesPer.append(resp)
+    responseProportions = []
+
+    for i in range(numQuestions): #gives the proportion of responses each answer choice was, for each question
+         #number of responses to question i should equal num. of users
+        proportions = [0]*numUsers 
+        numEachOption = [0]*(max(responsesPer[i]) + 1)# number of answer choices
+        for response in responsesPer[i]:
+            numEachOption[response] += 1 #response i => numeachOption[i] incremented; start at 0 
+        for j in range(len(numEachOption)):
+            proportions[j] = numEachOption[j]/numUsers
+        responseProportions.append(proportions)
+    
+    # for i in range(numQuestions):
+    #     means.append(sum(responsesPer[i])/numUsers) # mean response to q i 
+    #     stdevs.append(statistics.stdev(responsesPer[i])) #st deviation of responses to q i
+    #     modes.append(statistics.mode(responsesPer[i]))
+        
+
+    return responseProportions
+        
 
 # Takes in two user objects and outputs a float denoting compatibility
 def compute_score(user1, user2):
-    # YOUR CODE HERE
-    return 0
+    if not ((user1.gender in user2.preferences) and (user2.gender in user1.preferences)):
+        return 0 #incompatible sexualities
+    numqs = len(user1.responses)
+    answerCompatability = 0 
+    for i in range(numqs): 
+        if user1.responses[i] == user2.responses[i]:
+            answerCompatability += 1/(1 + stats[i][user1.responses[i]]) #at most 1 
+        else: 
+            answerCompatability = max(0,answerCompatability - (1/numqs)*(1/(1 + math.sqrt(stats[i][user1.responses[i]] * stats[i][user2.responses[i]]))))
+
+    
+    diff = abs(user1.grad_year - user2.grad_year)
+    if diff == 0: 
+        answerCompatability += 1 #same as 1 identical answers with high weight
+    elif diff == 1:
+        answerCompatability += .5 #same as 1 identical answer with average weight
+    
+    answerCompatability = answerCompatability / (numqs + 1) #max compat. is numqs + 1 (with grade boost); normalize
+
+    
+    return answerCompatability
 
 
 if __name__ == '__main__':
@@ -34,6 +86,9 @@ if __name__ == '__main__':
                             user_obj['preferences'], user_obj['gradYear'],
                             user_obj['responses'])
             users.append(new_user)
+
+    stats = calculate_stats() #placed in main to minimize runtime contribution 
+    print(stats)
 
     for i in range(len(users)-1):
         for j in range(i+1, len(users)):
